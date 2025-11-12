6cf6db95df78ecb6eedfe3a804ba23e660af1756
Joel Moye <joel.moye@strathmore.edu>
2025-11-12 17:25:45 +0300

    Refactor UserSeeder to include user creation logic

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -8,4 +8,27 @@ use App\Models\User;
 
 class UserSeeder extends Seeder
 {
-   
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@edubridge.com',
+            'password' => Hash::make('password'),
+            'role' => 'Admin',
+        ]);
+
+        User::create([
+            'name' => 'Organization User',
+            'email' => 'org@edubridge.com',
+            'password' => Hash::make('password'),
+            'role' => 'Organization',
+        ]);
+
+        User::create([
+            'name' => 'Youth User',
+            'email' => 'youth@edubridge.com',
+            'password' => Hash::make('password'),
+            'role' => 'Youth',
+        ]);
+    }
+}
